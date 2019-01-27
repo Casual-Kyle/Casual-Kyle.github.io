f00864a5a8c28498bd2de05641d04ff5724466c8
Kyle J <44511301+Casual-Kyle@users.noreply.github.com>
2019-01-26 19:01:01 -0600

    adjusted my new dropdown menu so it works... better.... It's liveable but maybe after programming a little while longer I'll get an idea of how to fix it, or make it better.

--- index.js
+++ index.js
@@ -7,12 +7,10 @@ on the side of the menu used for social media links and such
 
 function dropdownOver(){
 	var hiddenBox = document.getElementsByClassName("dropdown-content");
-	console.log(hiddenBox);
 	hiddenBox[0].style.display = 'block';
 };
 
 function dropdownOut(){
 	var hiddenBox = document.getElementsByClassName("dropdown-content");
-	console.log(hiddenBox);
 	hiddenBox[0].style.display = 'none';
 };
