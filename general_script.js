6f8955740ece65cb1778a65c7abb7e3ff15d3b6e
Kyle J <44511301+Casual-Kyle@users.noreply.github.com>
2019-01-29 21:36:34 -0600

    Added new code for finishing the D&D random stat gen function. It's almost finished, but the stat rolling for indivial functions is finished, as well as the stat roll for all fucntions is finished. Working on displaying recommended Class/s

--- general_script.js
+++ general_script.js
@@ -100,7 +100,6 @@ function diceRoller(){
 
 function statGenRandom(obj){
 	var buttonPressed = obj.id;
-	console.log("Button That Was Pressed: " + buttonPressed);
 
 	var strengthNum 	= document.getElementById("strNum");
 	var constitutionNum = document.getElementById("conNum");
@@ -116,30 +115,173 @@ function statGenRandom(obj){
 	var charismaBtn 	= document.getElementById("chaBtn").id;
 	var wisdomBtn 		= document.getElementById("wisBtn").id;
 
+	var allBtn 	= document.getElementById("allStatsBtn").id;
 
-	console.log("buttonPressed: "+buttonPressed);
-	console.log("strengthBtn: "+strengthBtn);
+	//console.log(buttonPressed, allBtn);
 
-	if (buttonPressed == strengthBtn){
-		console.log("strengthBtn was pressed");
-		if (strengthNum.innerHTML == "strBtn P'd"){
-			strengthNum.innerHTML = "20 1";
-		} else {
-			strengthNum.innerHTML = "strBtn P'd";
+	function threeDiceTotal(){
+		var rolledNumbers = [];
+		for (var i = 0; i < 4; i++){
+			var rollNum = (Math.floor(Math.random()*6)) + 1
+			rolledNumbers.push(rollNum);
 		};
-	} else if (buttonPressed == constitutionBtn){
-		console.log("constitutionBtn was pressed");
-	} else if (buttonPressed == dexterityBtn){
-		console.log("dexterityBtn was pressed");
-	} else if (buttonPressed == intelligenceBtn){
-		console.log("intelligenceBtn was pressed");
-	} else if (buttonPressed == charismaBtn){
-		console.log("charismaBtn was pressed");
-	} else if (buttonPressed == charismaBtn){
-		console.log("wisdomBtn was pressed");
+		rolledNumbers = rolledNumbers.sort(function(a,b){return b-a});
+		//console.log(rolledNumbers);
+
+		rolledNumbers.pop();
+		//console.log(rolledNumbers);
+
+		var statNum = (rolledNumbers[0]) + (rolledNumbers[1]) + (rolledNumbers[2]); 
+		return statNum;
 	};
 
 
 
-	//strengthNum.innerHTML = "this_is_me"
+	var strStat = strengthNum.innerHTML;
+	var conStat = constitutionNum.innerHTML;
+	var dexStat = constitutionNum.innerHTML;
+	var intStat = constitutionNum.innerHTML;
+	var chaStat = constitutionNum.innerHTML;
+	var wisStat = constitutionNum.innerHTML;
+
+
+	if (buttonPressed == allBtn){
+		var btnArray = [strengthNum,
+						constitutionNum,
+						dexterityNum,
+						intelligenceNum,
+						charismaNum,
+						wisdomNum];
+		for (var c = 0; c < 6; c++){
+			var statHTML 	= btnArray[c];
+			var statNum 	= threeDiceTotal();
+			statHTML.innerHTML = statNum;
+
+			if (c == 0){
+				strStat = statNum;
+			} else if (c == 1){
+				conStat = statNum;
+			} else if (c == 2){
+				dexStat = statNum;
+			} else if (c == 3){
+				intStat = statNum;
+			} else if (c == 4){
+				chaStat = statNum;
+			} else if (c == 5){
+				wisStat = statNum;
+			};
+
+		};
+
+	} else {
+
+		var statNum = threeDiceTotal();
+
+		if (buttonPressed == strengthBtn){
+			//console.log("strengthBtn was pressed");
+			strengthNum.innerHTML = statNum;
+			
+		} else if (buttonPressed == constitutionBtn){
+			//console.log("constitutionBtn was pressed");
+			constitutionNum.innerHTML = statNum;
+			
+		} else if (buttonPressed == dexterityBtn){
+			//console.log("dexterityBtn was pressed");
+			dexterityNum.innerHTML = statNum;
+			
+		} else if (buttonPressed == intelligenceBtn){
+			//console.log("intelligenceBtn was pressed");
+			intelligenceNum.innerHTML = statNum;
+			
+		} else if (buttonPressed == charismaBtn){
+			//console.log("charismaBtn was pressed");
+			charismaNum.innerHTML = statNum;
+			
+		} else if (buttonPressed == wisdomBtn){
+			//console.log("wisdomBtn was pressed");
+			wisdomNum.innerHTML = statNum;
+			
+		};
+
+
+		strStat = strengthNum.innerHTML;
+		conStat = constitutionNum.innerHTML;
+		dexStat = dexterityNum.innerHTML;
+		intStat = intelligenceNum.innerHTML;
+		chaStat = charismaNum.innerHTML;
+		wisStat = wisdomNum.innerHTML;
+
+
+		
+
+
+
+
+	};
+
+	console.log("Stats: ", strStat, conStat, dexStat, intStat, chaStat, wisStat);
+	
+
+	var statArray = [strStat,
+						conStat,
+						dexStat,
+						intStat,
+						chaStat,
+						wisStat];
+
+
+	console.log("statArray: ", statArray);
+
+
+	statArray.sort(function(a,b){return b-a});
+	console.log("sorted array: ", statArray);
+
+	var highestStat = statArray[0];
+	var highStatsArray = []; //might be relevant, maybe not
+	console.log("highest: ", highestStat)
+
+	var secHighestStat = 0
+	var secHighStatsArray = []; //might be relevant, maybe not
+
+	if (statArray[1] == highestStat){
+		if (statArray[2] == highestStat){
+			secHighestStat = statArray[3];
+		} else {
+			secHighestStat = statArray[2];
+		};
+		
+	} else {
+		secHighestStat = statArray[1];
+	};
+
+
+	for (var x = 0; x < 6; x++){
+		if (statArray[x] == highestStat){
+			console.log("stat vs, highest stat: ", statArray[x], highestStat);
+			highStatsArray.push(statArray[x]);
+		};
+
+		if (statArray[x] != highestStat && statArray[x] == secHighestStat){
+			console.log("secStat vs, second stat: ", statArray[x], secHighestStat);
+			secHighStatsArray.push(statArray[x]);
+		};
+	};
+
+
+
+
+	console.log("High Stats: ", highStatsArray);
+	console.log("Secon High: ", secHighStatsArray);
+
+	/*
+		basically if highestStat == strStat
+			if secondHigh stat == conStat
+				barbarian
+			elif secondHigh stat == wisStat
+				paladin
+			elif secondHigh stat == intStat
+				fighter - arcane?
+	*/
+
+
 };
